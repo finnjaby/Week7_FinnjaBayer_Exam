fdaa2e77a88cbbeebffc307a83b5b112361cb870
FinnjaBayer <g00470359@atu.ie>
2025-11-05 16:27:40 +0000

    added main customer management class, created menu message

---
+++ src/main/java/ie/atu/CustomerManagement.java
@@ -0,0 +1,12 @@
+package ie.atu;
+import java.util.ArrayList;
+
+public class CustomerManagement{
+    public static void main(String[] args){
+        System.out.println("=== Customer Management System ===");
+        System.out.println("1) Add Customer");
+        System.out.println("2) Update Customer");
+        System.out.println("3) Display All");
+        System.out.println("4) Exit");
+    }
+}
