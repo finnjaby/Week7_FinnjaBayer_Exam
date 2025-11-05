ed4961bda86277ce5bc71a0939c344d46d1896f6
FinnjaBayer <g00470359@atu.ie>
2025-11-05 16:31:40 +0000

    added option for user to choose from menu

--- src/main/java/ie/atu/CustomerManagement.java
+++ src/main/java/ie/atu/CustomerManagement.java
@@ -1,5 +1,6 @@
 package ie.atu;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CustomerManagement{
     public static void main(String[] args){
@@ -8,5 +9,12 @@ public class CustomerManagement{
         System.out.println("2) Update Customer");
         System.out.println("3) Display All");
         System.out.println("4) Exit");
+
+        System.out.println("Please pick an option 1-4");
+        int input;
+        Scanner sc = new Scanner(System.in);
+        input = sc.nextInt();
+
+
     }
 }
